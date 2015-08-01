e6bac89aa063e64fbac6977bb12b273222177555
russelleh <russell.west.eh@gmail.com>
2015-08-01 17:05:22 -0600

    It returns all instances as an array

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -8,4 +8,12 @@ class InvoiceRepositoryTest < Minitest::Test
 
     assert_kind_of Repository, repo
   end
+
+  def test_it_returns_all_instances_as_an_array
+    engine = SalesEngine.new
+    engine.startup
+    repo = engine.invoice_repository
+
+    assert_kind_of Array, repo.all
+  end
 end
