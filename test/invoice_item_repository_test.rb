1422e191ea1e25b401147125dd6a8b53910492e9
russelleh <russell.west.eh@gmail.com>
2015-08-01 16:27:10 -0600

    Can return all instances

--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -18,4 +18,11 @@ class InvoiceItemRepositoryTest < Minitest::Test
 
     assert_kind_of Array, engine.invoice_item_repository.all
   end
+
+  def test_can_return_all_instances
+    engine = SalesEngine.new
+    engine.startup
+
+    assert engine.invoice_item_repository.all.length >= 1000
+  end
 end
