a4bd2c252ab8f0732bf209a99f6e75baecdb63a5
russelleh <russell.west.eh@gmail.com>
2015-08-03 14:34:24 -0600

    Customer search

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -2,4 +2,12 @@ class CustomerRepository < Repository
   def find_by_last_name value
     find_by(:last_name, value)
   end
+
+  def find_all_by_first_name value
+    find_all_by(:first_name, value)
+  end
+
+  def invoices customer
+    customer.invoices
+  end
 end
