129a5e075c63b4d562dfb317acf20e5884aff762
russelleh <russell.west.eh@gmail.com>
2015-08-03 17:52:03 -0600

    Remove douplicate code in find_by and find_all_by

--- lib/repository.rb
+++ lib/repository.rb
@@ -15,11 +15,7 @@ class Repository
   end
 
   def find_by(field, value)
-    if records[0].respond_to?(field)
-      records.find {|record| (record.send field).to_s.downcase == value.to_s.downcase}
-    else
-      raise ArgumentError, "Attempted to locate a record by '#{field}', but that isn't a valid field for #{records[0].class} objects."
-    end
+    find_all_by(field, value)[0]
   end
 
   def find_all_by(field, value)
