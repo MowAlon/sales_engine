19423e112bd6cee3776d2e0a77f43fa9ef7b89d9
russelleh <russell.west.eh@gmail.com>
2015-08-03 18:20:00 -0600

    Change inoice repository attribute to repository attribute

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,10 +1,10 @@
 require_relative 'data_instance'
 
 class Invoice < DataInstance
-  attr_reader :invoice_repository, :customer_id, :merchant_id, :status
+  attr_reader :repository, :customer_id, :merchant_id, :status
 
-  def initialize(invoice, invoice_repository)
-    @invoice_repository = invoice_repository
+  def initialize(invoice, repository)
+    @repository = repository
     @id = invoice[0]
     @customer_id = invoice[1]
     @merchant_id = invoice[2]
@@ -15,28 +15,28 @@ class Invoice < DataInstance
 
   def transactions
     # returns a collection of associated Transaction instances
-    invoice_repository.sales_engine.transaction_repository.find_all_by(:invoice_id, id)
+    repository.sales_engine.transaction_repository.find_all_by(:invoice_id, id)
   end
 
   def invoice_items
     # returns a collection of associated InvoiceItem instances
-    invoice_repository.sales_engine.invoice_item_repository.find_all_by(:invoice_id, id)
+    repository.sales_engine.invoice_item_repository.find_all_by(:invoice_id, id)
   end
 
   def items
     # returns a collection of associated Items by way of InvoiceItem objects
     item_ids = invoice_items.map {|invoice_item| invoice_item.item_id}.uniq
-    item_ids.map {|item_id| invoice_repository.sales_engine.item_repository.find_by(:id, item_id)}
+    item_ids.map {|item_id| repository.sales_engine.item_repository.find_by(:id, item_id)}
   end
 
   def customer
     # returns an instance of Customer associated with this object
-    invoice_repository.sales_engine.customer_repository.find_by(:id, customer_id)
+    repository.sales_engine.customer_repository.find_by(:id, customer_id)
   end
 
   def merchant
     # returns an instance of Merchant associated with this object
-    invoice_repository.sales_engine.merchant_repository.find_by(:id, merchant_id)
+    repository.sales_engine.merchant_repository.find_by(:id, merchant_id)
   end
 
 end
